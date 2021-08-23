491af93377ca5359646ad19c487cfc5ea4ca79e5
daix6 <daixuan1996@gmail.com>
2021-08-23 22:41:21 +0800

    feat: fix appletv and add hulu

--- tv_wiki/appletv.js
+++ tv_wiki/appletv.js
@@ -1,6 +1,5 @@
 const { runStreamNetwork } = require('./general');
 const fs = require('fs');
-const moment = require('moment');
 
 const WIKI_NAME = 'List of Apple TV+ original programming';
 
@@ -11,13 +10,22 @@ async function runAll() {
 }
 
 async function runAiredTV() {
-  const result = filterUpcoming(filterUnscriptedAndFilm(await runStreamNetwork(WIKI_NAME)));
+  const result = filterUpcoming(filterUnscripted(filterFilm(await runStreamNetwork(WIKI_NAME))));
 
   await fs.promises.writeFile(require('path').join(__dirname, `dist/appletv+_tv_aired_${Date.now()}.json`), JSON.stringify(result, undefined, 2), { encoding: 'utf-8' });
 }
 
-function filterUnscriptedAndFilm(category) {
-  return category.filter(row => !['unscripted', 'original films'].includes(row.category[0].toLowerCase()));
+function filterUnscripted(category) {
+  return category.filter(row => {
+    const cate = row.category[1].toLowerCase();
+    const subCate = row.category[2]?.toLowerCase();
+    return  !['unscripted'].includes(cate) || ['docuseries'].includes(subCate);
+  });
+}
+
+
+function filterFilm(category) {
+  return category.filter(row => !['original films'].includes(row.category[0].toLowerCase()));
 }
 
 function filterUpcoming(category) {
---
+++ tv_wiki/hulu.js
@@ -0,0 +1,42 @@
+const { runStreamNetwork } = require('./general');
+const fs = require('fs');
+
+const WIKI_NAME = 'List of Hulu original programming';
+
+async function runAll() {
+  const result = filterExclusive(await runStreamNetwork(WIKI_NAME));
+
+  await fs.promises.writeFile(require('path').join(__dirname, `dist/hulu_all_${Date.now()}.json`), JSON.stringify(result, undefined, 2), { encoding: 'utf-8' });
+}
+
+
+async function runAiredTV() {
+  const result = filterUpcoming(filterUnscripted(filterFilm(filterExclusive(await runStreamNetwork(WIKI_NAME)))));
+
+  await fs.promises.writeFile(require('path').join(__dirname, `dist/hulu_tv_aired_${Date.now()}.json`), JSON.stringify(result, undefined, 2), { encoding: 'utf-8' });
+}
+
+function filterExclusive(category) {
+  return category.filter(row => !['exclusive international television distribution'].includes(row.category[0].toLowerCase()));
+}
+
+function filterUnscripted(category) {
+  return category.filter(row => {
+    const cate = row.category[1].toLowerCase();
+    const subCate = row.category[2]?.toLowerCase();
+    return  !['unscripted'].includes(cate) || ['docuseries'].includes(subCate);
+  });
+}
+
+
+function filterFilm(category) {
+  return category.filter(row => !['original films', 'co-distributed films'].includes(row.category[0].toLowerCase()));
+}
+
+function filterUpcoming(category) {
+  return category.filter(row => !['upcoming original programming', 'upcoming original films'].includes(row.category[0].toLowerCase()));
+}
+
+if (!module.parent) {
+  runAiredTV();
+}
