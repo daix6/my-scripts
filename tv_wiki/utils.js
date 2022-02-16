cb3a28bf46852f124d805f2df75dd4b889970195
daix6 <daixuan1996@gmail.com>
2022-02-16 20:47:02 +0800

    feat: update

--- tv_wiki/utils.js
+++ tv_wiki/utils.js
@@ -9,11 +9,13 @@ function parseTable(tableEl) {
   let [thead, ...rows] = tableEl.querySelectorAll('tr');
 
   const headers = thead.querySelectorAll('th').map(el => removeUnicode(el.rawText.trim()));
-  rows = rows.filter(row => !row.querySelector('th'));
+  rows = rows.filter(row => {
+    return row.querySelectorAll('th,td').length > 1;
+  });
 
   const result = rows.map(row => Array(headers.length));
   rows.forEach((row, rowIndex) => {
-    row.querySelectorAll('td').forEach((td, _colIndex) => {
+    row.querySelectorAll('th,td').forEach((td, _colIndex) => {
       let colIndex = _colIndex;
 
       for (colIndex = _colIndex; colIndex < result[rowIndex].length; colIndex++) {
@@ -26,7 +28,7 @@ function parseTable(tableEl) {
       // 有些情况 wiki 的 table 的 rowspan 会写错。。
       for (let i = 0; i < rowspan && rowIndex + i < result.length; i++) {
         for (let j = 0; j < colspan && colIndex + j < headers.length; j++) {
-          result[rowIndex + i][colIndex + j] = removeUnicode(td.rawText.trim());
+          result[rowIndex + i][colIndex + j] = removeUnicode(td.rawText.trim().replace(/(&#91;\w+&#93;)+/, ''));
         }
       }
     });
